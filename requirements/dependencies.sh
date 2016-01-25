3bdf4a2fe564e35edb53849cf68ddeabbf0912ee
Tomas Pllaha <tomas@cliqz.com>
2016-01-25 14:09:28 +0100

    Added dependencies

---
+++ requirements/dependencies.sh
@@ -0,0 +1,12 @@
+apt-get install -y libpng-dev
+apt-get install -y dh-autoreconf
+apt-get install -y libspatialindex-dev
+apt-get install -y libgeos-dev
+apt-get install -y libffi-dev
+apt-get install -y libssl-dev
+apt-get install -y gfortran
+apt-get install -y libblas-dev liblapack-dev
+apt-get install -y mysql-client
+add-apt-repository -y ppa:maxmind/ppa
+apt-get update
+apt-get install -y libmaxminddb0 libmaxminddb-dev mmdb-bin
